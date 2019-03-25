ed483142cf1f9822a91bd72c1a12403d1de45eb6
Jennifer Halloran <jenniferlhalloran@gmail.com>
2019-03-25 21:23:50 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,10 +1,1 @@
 var chocolateBars = [ "snickers", "hundred grand", "kitkat", "skittles"]
-
-function addElementToBeginningOfArray(array, element){
-  var array = [ "one", "two", "three"]
-  array.unshift("zero")
-}
-
-function destructivelyAddElementToBeginningOfArray(array, element) {
-
-}
