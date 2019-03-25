fe5f3988cf2501938bc1b74cd06cfd32b29981f6
Jennifer Halloran <jenniferlhalloran@gmail.com>
2019-03-25 18:49:30 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,10 @@
+var chocolateBars = [ "snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray(array, element){
+  var array = [ "one", "two", "three"]
+  array.push("zero")
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+
+}
